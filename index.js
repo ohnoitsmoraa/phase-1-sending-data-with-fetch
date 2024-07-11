0ad22a3ad57b254206f8563405790cd8fc3b8577
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-07-11 22:25:00 +0300

    Modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      const newId = data.id;
+      const idElement = document.createElement("p");
+      idElement.textContent = newId;
+      document.body.appendChild(idElement);
+    })
+
+    .catch((error) => {
+      const idElement = document.createElement("p");
+      idElement.textContent = `Error: ${error.message}`;
+      document.body.appendChild(idElement);
+    });
+}
